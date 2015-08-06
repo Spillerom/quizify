9b2c63f212649f093ab70ac468d2a6d50f59388f
Hans-Petter Harveg <hanspetter@spillerom.no>
2015-08-06 15:52:42 +0200

    Added feedback strings to the alternatives

--- language/no/questions.php
+++ language/no/questions.php
@@ -6,14 +6,23 @@ $questions = [
             [
                 'text' => 'Sendt Harald Eia til frisøren',
                 'score' => '0,0,0,0,0,0,0,0,0,0,0,0,0',
+                'feedback' => [
+                	'text' = 'Lykke til'
+                ]
             ],
             [
                 'text' => 'Sneket deg med på innspilling av Åndenes makt for å se om spøkelsene er ekte',
                 'score' => '0,0,0,0,0,0,0,0,0,0,0,0,0'
+                'feedback' => [
+                	'text' = 'Tom Strømness lover å ikke skremme deg med lommelykt under haka'
+                ]
             ],
             [
                 'text' => 'Gitt Magnus Devold en ordentilig TV-jobb',
                 'score' => '0,0,0,0,0,0,0,0,0,0,0,0,0'
+                'feedback' => [
+                	'text' = 'Voilà. Ditt ord er lov. Kjendiskveld med Magnus Devold har premiere til høsten'
+                ]
             ]
         ]
     ],
@@ -23,14 +32,23 @@ $questions = [
             [
                 'text' => 'At altfor få fremmede gifter seg på TV uten å møtes på forhånd',
                 'score' => '0,0,0,0,0,0,0,0,0,0,0,0,0',
+                'feedback' => [
+                	'text' = 'Paradise Hotel burde ta ansvar og arrangere flere bryllup, spør du oss'
+                ]
             ],
             [
-                'text' => 'At du ikke ve hva Knut Nærum gjør en helt vanlig dag',
+                'text' => 'At du ikke vet hva Knut Nærum gjør en helt vanlig dag',
                 'score' => '0,0,0,0,0,0,0,0,0,0,0,0,0'
+                'feedback' => [
+                	'text' = 'Knut leser sikkert tegneserier på senga hele dagen'
+                ]
             ],
             [
                 'text' => 'At håndverkere på TV er generelt for blide',
                 'score' => '0,0,0,0,0,0,0,0,0,0,0,0,0'
+                'feedback' => [
+                	'text' = 'Vi har nummeret til noen mugne rørleggere hvis du trenger'
+                ]
             ]
         ]
     ],
@@ -40,14 +58,23 @@ $questions = [
             [
                 'text' => 'Rødvin og muggost som er vrien på både uttale og smak',
                 'score' => '0,0,0,0,0,0,0,0,0,0,0,0,0',
+                'feedback' => [
+                	'text' = 'Hva med en flaske Alary La Font d´Estévenas og en Bleu de Gex, din snobb'
+                ]
             ],
             [
                 'text' => 'Ostepop og barnevakt',
                 'score' => '0,0,0,0,0,0,0,0,0,0,0,0,0'
+                'feedback' => [
+                	'text' = 'Eller en annen type snacks som våre annonsører reklamerer for (produktplasseringsP)'
+                ]
             ],
             [
                 'text' => 'Snapchat og multitasking',
                 'score' => '0,0,0,0,0,0,0,0,0,0,0,0,0'
+                'feedback' => [
+                	'text' = 'Psst! Sjekk tvnorgesnap'
+                ]
             ]
         ]
     ],
@@ -57,14 +84,23 @@ $questions = [
             [
                 'text' => 'Nja, hvorfor ikke?',
                 'score' => '0,0,0,0,0,0,0,0,0,0,0,0,0',
+                'feedback' => [
+                	'text' = 'Tom vet å by på seg selv. Du skulle vært med på julebordet i fjor'
+                ]
             ],
             [
                 'text' => 'Kan jeg by på litt rødvin og muggost som er vrien på både uttale og smak?',
                 'score' => '0,0,0,0,0,0,0,0,0,0,0,0,0'
+                'feedback' => [
+                	'text' = 'Tom er nok litt mer på primus og pinnebrød-kjøret, men hvem vet?'
+                ]
             ],
             [
                 'text' => 'Hvem slapp inn Stalker-Stiansen?!? ALARM! ALARM!',
                 'score' => '0,0,0,0,0,0,0,0,0,0,0,0,0'
+                'feedback' => [
+                	'text' = 'Hvis du bare åpner vinduet så flyr han ut av seg selv'
+                ]
             ]
         ]
     ],
@@ -74,14 +110,23 @@ $questions = [
             [
                 'text' => 'OMG! (emoji emoji emoji) jeg er admin på Kløftas største Ylvis-fansie på Instagram. Er bilettene til liveshowet lagt ut? (emoji)',
                 'score' => '0,0,0,0,0,0,0,0,0,0,0,0,0',
+                'feedback' => [
+                	'text' = 'Keep calm and sjekk ut billettservice.no'
+                ]
             ],
             [
                 'text' => 'Næh, de var bedre før da de var indie i Bergen',
                 'score' => '0,0,0,0,0,0,0,0,0,0,0,0,0'
+                'feedback' => [
+                	'text' = '90-tallet ringte. De vil gjerne ha grungejakka og tonefallet sitt tilbake'
+                ]
             ],
             [
-                'text' => 'Hva er en Ylvis og hva e en OMG?',
+                'text' => 'Hva er en Ylvis og hva er en OMG?',
                 'score' => '0,0,0,0,0,0,0,0,0,0,0,0,0'
+                'feedback' => [
+                	'text' = 'Det er mye fint fjernsynsteater på NRK 2'
+                ]
             ]
         ]
     ],
@@ -91,14 +136,23 @@ $questions = [
             [
                 'text' => 'Et spennende sosialt eksperiment',
                 'score' => '0,0,0,0,0,0,0,0,0,0,0,0,0',
+                'feedback' => [
+                	'text' = 'Hmmm. Den har vi hørt før'
+                ]
             ],
             [
                 'text' => 'Starten på slutten for vanlig verdighet',
                 'score' => '0,0,0,0,0,0,0,0,0,0,0,0,0'
+                'feedback' => [
+                	'text' = 'Programmet er faktisk kleinest når de kler på seg på slutten'
+                ]
             ],
             [
                 'text' => 'Påvirker denne testen min søknad om å være med i Adam søker Eva?',
                 'score' => '0,0,0,0,0,0,0,0,0,0,0,0,0'
+                'feedback' => [
+                	'text' = 'Neida, joda, neida'
+                ]
             ]
         ]
     ],
@@ -108,14 +162,23 @@ $questions = [
             [
                 'text' => 'Et spennende sosialt eksperiment',
                 'score' => '0,0,0,0,0,0,0,0,0,0,0,0,0',
+                'feedback' => [
+                	'text' = 'Det er kanskje å ta det litt langt?'
+                ]
             ],
             [
                 'text' => 'En livsglad gjeng i campigvogner på svenskegrensa',
                 'score' => '0,0,0,0,0,0,0,0,0,0,0,0,0'
+                'feedback' => [
+                	'text' = 'Så du har vært der?'
+                ]
             ],
             [
                 'text' => 'Sakte-TV fra et snobbete boligstrøk på Oslo vest',
                 'score' => '0,0,0,0,0,0,0,0,0,0,0,0,0'
+                'feedback' => [
+                	'text' = 'Ikke prøv å selge ideen til NRK. Vi kom på den først'
+                ]
             ]
         ]
     ],
@@ -123,33 +186,51 @@ $questions = [
         'question' => 'Hvor i landet hadde du sendt Alt for Norge-deltakerne?',
         'alternatives' => [
             [
-                'text' => 'Et sted med duvende budeier og nasjonalromantisk overkill',
+                'text' => 'Et sted med duvende budeier og nasjonalromantisk overdose',
                 'score' => '0,0,0,0,0,0,0,0,0,0,0,0,0',
+                'feedback' => [
+                	'text' = 'Prøver du å smiske nå?'
+                ]
             ],
             [
-                'text' => 'Montebello Camping',
+                'text' => 'Hjem til meg',
                 'score' => '0,0,0,0,0,0,0,0,0,0,0,0,0'
+                'feedback' => [
+                	'text' = 'Da får du ikke lov til å lære dem altfor stygge banneord'
+                ]
             ],
             [
                 'text' => 'Til en helt vanlig dag hos Petter Schjerven',
                 'score' => '0,0,0,0,0,0,0,0,0,0,0,0,0'
+                'feedback' => [
+                	'text' = 'Ikke spør oss om hva det innebærer'
+                ]
             ]
         ]
     ],
     [
-        'question' => 'Hva er den beste programkombinasjonen av disse forslagene?',
+        'question' => 'Hva synes du er den beste programkombinasjonen av disse forslagene?',
         'alternatives' => [
             [
-                'text' => 'Sinnasnekkern gifter seg ved første blikk',
+                'text' => 'Sinnasnekker`n gifter seg ved første blikk',
                 'score' => '0,0,0,0,0,0,0,0,0,0,0,0,0',
+                'feedback' => [
+                	'text' = 'De seertallene kan vi bare drømme om'
+                ]
             ],
             [
                 'text' => 'Åndenes kjendiskveld',
                 'score' => '0,0,0,0,0,0,0,0,0,0,0,0,0'
+                'feedback' => [
+                	'text' = 'Elvis og 2Pac inviterer'
+                ]
             ],
             [
-                'text' => 'Alt for Asbjørn brekke',
+                'text' => '71° Thomas Giertsen',
                 'score' => '0,0,0,0,0,0,0,0,0,0,0,0,0'
+                'feedback' => [
+                	'text' = 'Var det seriøst noen som valgte denne?'
+                ]
             ]
         ]
     ],
@@ -159,14 +240,23 @@ $questions = [
             [
                 'text' => 'Reality',
                 'score' => '0,0,0,0,0,0,0,0,0,0,0,0,0',
+                'feedback' => [
+                	'text' = 'Sorry. Det blir ikke noe mer av Den siste viking'
+                ]
             ],
             [
                 'text' => 'Humor',
                 'score' => '0,0,0,0,0,0,0,0,0,0,0,0,0'
+                'feedback' => [
+                	'text' = 'Hvordan vet du at en bil er fra Jamaica? På REGGAEstreringsskiltet!'
+                ]
             ],
             [
                 'text' => 'Underholdning',
                 'score' => '0,0,0,0,0,0,0,0,0,0,0,0,0'
+                'feedback' => [
+                	'text' = 'Det var hyggelig av deg å svare'
+                ]
             ]
         ]
     ]
