127871cf794621bd85cc397a259abbb89eaf49f9
Hans-Petter Harveg <hanspetter@spillerom.no>
2015-08-12 12:39:00 +0200

    Added new questin files

---
+++ language/no/questions_2.php
@@ -0,0 +1,134 @@
+<?php
+$questions = [
+	[
+        'question' => 'Hvem i TVNorge-universet er kjent for uttrykk som «Legendarisk smud» og «Bænkers ordnings»?',
+        'alternatives' => [
+            [
+                'text' => '«Alexander Molteberg spilt av Lene Kongsvik Johansen', 
+                'score' => '0,0,0,0,0,0,0,0,0,0,0,0,0',
+                'feedback' => [
+                	'text' = 'Korrekt, eller «sykt stålkontroll» som Alexander ville sagt'
+                ]
+            ],
+            [
+                'text' => 'Ine Jansen på TVNorges julebord i 2012',
+                'score' => '0,0,0,0,0,0,0,0,0,0,0,0,0'
+                'feedback' => [
+                	'text' = 'Bare lat som ingenting hvis du møter henne'
+                ]
+            ],
+            [
+                'text' => 'Jarle Andhøy snøvla det da han slentra forbi kontoret i stad',
+                'score' => '0,0,0,0,0,0,0,0,0,0,0,0,0'
+                'feedback' => [
+                	'text' = 'Fun fact: Vi har ikke hørt fra Jarle etter han dro til Antarktis med Anne Kat.(emoji)'
+                ]
+            ]
+        ]
+	],
+	[
+        'question' => 'Hvilket av disse programmene tror du hadde fått høyest seertall?',
+        'alternatives' => [
+            [
+                'text' => 'Espen Eckbo rydder opp - hjemme',
+                'score' => '0,0,0,0,0,0,0,0,0,0,0,0,0',
+                'feedback' => [
+                	'text' = 'Gjeeesp'
+                ]
+            ],
+            [
+                'text' => '«Odda» og Henrik nakendater',
+                'score' => '0,0,0,0,0,0,0,0,0,0,0,0,0'
+                'feedback' => [
+                	'text' = 'Eventuelt Hver gang vi nakendater eller Skal vi nakendate'
+                ]
+            ],
+            [
+                'text' => 'Hvem kan slå Kristian Valen og Lilli Bendriss',
+                'score' => '0,0,0,0,0,0,0,0,0,0,0,0,0'
+                'feedback' => [
+                	'text' = 'Vi vippser deg 100 kroner hvis du tør å foreslå konseptet for Kristian og Lilli'
+                ]
+            ]
+        ]
+    ],
+    [
+        'question' => 'Hvilket utsagn beskriver deg best?',
+        'alternatives' => [
+            [
+                'text' => 'Det er altfor lite reklame på TV nå om da`n',
+                'score' => '0,0,0,0,0,0,0,0,0,0,0,0,0',
+                'feedback' => [
+                	'text' = 'Saklig. Det har du aldri sagt'
+                ]
+            ],
+            [
+                'text' => 'TVNorge er klart best på humor',
+                'score' => '0,0,0,0,0,0,0,0,0,0,0,0,0'
+                'feedback' => [
+                	'text' = 'Motta 1 000 000 poeng'
+                ]
+            ],
+            [
+                'text' => 'Jeg ser nesten ikke på TV lenger, jeg. Det er så mye bra på Netflix og Popcorn Time',
+                'score' => '0,0,0,0,0,0,0,0,0,0,0,0,0'
+                'feedback' => [
+                	'text' = 'Blah blah blah, vi hører ikke hva du sier'
+                ]
+            ]
+        ]
+    ],
+    [
+        'question' => 'Hvordan ser vi på TV om 10 år?',
+        'alternatives' => [
+            [
+                'text' => 'NRK planter kringkastingsmicrochips i nyfødte barn gjennom vaksineprogrammet, finansiert av økt lisensavgift',
+                'score' => '0,0,0,0,0,0,0,0,0,0,0,0,0',
+                'feedback' => [
+                	'text' = '#KonspirasjonNRK #TheTruthIsOutThere'
+                ]
+            ],
+            [
+                'text' => 'Google glasses som streamer Monster-HD med 17G',
+                'score' => '0,0,0,0,0,0,0,0,0,0,0,0,0'
+                'feedback' => [
+                	'text' = 'Pornobransjen må bare bli enig om standard format først'
+                ]
+            ],
+            [
+                'text' => 'Cirka sånn som vi ser på TV i dag',
+                'score' => '0,0,0,0,0,0,0,0,0,0,0,0,0'
+                'feedback' => [
+                	'text' = 'Det høres koseligst ut'
+                ]
+            ]
+        ]
+    ],
+    [
+        'question' => 'Hva hadde du savnet mest på en øde øy?',
+        'alternatives' => [
+            [
+                'text' => 'En blind date uten klær',
+                'score' => '0,0,0,0,0,0,0,0,0,0,0,0,0',
+                'feedback' => [
+                	'text' = 'Sliter du på Tinder? (Eventuelt Grindr)'
+                ]
+            ],
+            [
+                'text' => 'Skamløs kjendisjournalistikk',
+                'score' => '0,0,0,0,0,0,0,0,0,0,0,0,0'
+                'feedback' => [
+                	'text' = 'Se og Hør kommer til å fremstå som IKEA-katalogen når Kjendiskveld med Magnus Devold går på lufta'
+                ]
+            ],
+            [
+                'text' => 'Lokkene til Vegard Ylvisåker',
+                'score' => '0,0,0,0,0,0,0,0,0,0,0,0,0'
+                'feedback' => [
+                	'text' = 'Han lukter godt også FYI'
+                ]
+            ]
+        ]
+    ]
+];
+?>
---
+++ language/no/questions_3.php
@@ -0,0 +1,104 @@
+<?php
+$questions = [
+    [
+        'question' => 'Hvilke ville du gjort hvis du var TVNorge-sjef en dag?',
+        'alternatives' => [
+            [
+                'text' => 'Sendt Harald Eia til frisøren',
+                'score' => '0,0,0,0,0,0,0,0,0,0,0,0,0',
+                'feedback' => 'Lykke til'
+            ],
+            [
+                'text' => 'Sneket deg med på innspilling av Åndenes makt for å se om spøkelsene er ekte',
+                'score' => '0,0,0,0,0,0,0,0,0,0,0,0,0',
+                'feedback' => 'Tom Strømness lover å ikke skremme deg med lommelykt under haka'
+            ],
+            [
+                'text' => 'Gitt Magnus Devold en ordentilig TV-jobb',
+                'score' => '0,0,0,0,0,0,0,0,0,0,0,0,0',
+                'feedback' => 'Voilà. Ditt ord er lov. Kjendiskveld med Magnus Devold har premiere til høsten'
+            ]
+        ]
+    ],
+    [
+        'question' => 'Hva irriterer deg når du ser på TV?',
+        'alternatives' => [
+            [
+                'text' => 'At altfor få fremmede gifter seg på TV uten å møtes på forhånd',
+                'score' => '0,0,0,0,0,0,0,0,0,0,0,0,0',
+                'feedback' => 'Paradise Hotel burde ta ansvar og arrangere flere bryllup, spør du oss'
+			],
+            [
+                'text' => 'At du ikke vet hva Knut Nærum gjør en helt vanlig dag',
+                'score' => '0,0,0,0,0,0,0,0,0,0,0,0,0',
+                'feedback' => 'Knut leser sikkert tegneserier på senga hele dagen'
+            ],
+            [
+                'text' => 'At håndverkere på TV er generelt for blide',
+                'score' => '0,0,0,0,0,0,0,0,0,0,0,0,0',
+                'feedback' => 'Vi har nummeret til noen mugne rørleggere hvis du trenger'
+            ]
+        ]
+    ],
+    [
+        'question' => 'Hvilken av de følgende kombinasjoner foretrekker du en kveld foran TV-skjermen?',
+        'alternatives' => [
+            [
+                'text' => 'Rødvin og muggost som er vrien på både uttale og smak',
+                'score' => '0,0,0,0,0,0,0,0,0,0,0,0,0',
+                'feedback' => 'Hva med en flaske Alary La Font d´Estévenas og en Bleu de Gex, din snobb'
+            ],
+            [
+                'text' => 'Ostepop og barnevakt',
+                'score' => '0,0,0,0,0,0,0,0,0,0,0,0,0',
+                'feedback' => 'Eller en annen type snacks som våre annonsører reklamerer for (produktplasseringsP)'
+            ],
+            [
+                'text' => 'Snapchat og multitasking',
+                'score' => '0,0,0,0,0,0,0,0,0,0,0,0,0',
+                'feedback' => 'Psst! Sjekk tvnorgesnap'
+            ]
+        ]
+    ],
+    [
+        'question' => 'Hva hadde du sagt hvis Tom Stiansen kom inn i rommet nå?',
+        'alternatives' => [
+            [
+                'text' => 'Nja, hvorfor ikke?',
+                'score' => '0,0,0,0,0,0,0,0,0,0,0,0,0',
+                'feedback' => 'Tom vet å by på seg selv. Du skulle vært med på julebordet i fjor'
+            ],
+            [
+                'text' => 'Kan jeg by på litt rødvin og muggost som er vrien på både uttale og smak?',
+                'score' => '0,0,0,0,0,0,0,0,0,0,0,0,0',
+                'feedback' => 'Tom er nok litt mer på primus og pinnebrød-kjøret, men hvem vet?'
+            ],
+            [
+                'text' => 'Hvem slapp inn Stalker-Stiansen?!? ALARM! ALARM!',
+                'score' => '0,0,0,0,0,0,0,0,0,0,0,0,0',
+                'feedback' => 'Hvis du bare åpner vinduet så flyr han ut av seg selv'
+            ]
+        ]
+    ],
+    [
+        'question' => 'Ylvis er tilbake på skjermen i høst. Hva tenker du om det?',
+        'alternatives' => [
+            [
+                'text' => 'OMG! (emoji emoji emoji) jeg er admin på Kløftas største Ylvis-fansie på Instagram. Er bilettene til liveshowet lagt ut? (emoji)',
+                'score' => '0,0,0,0,0,0,0,0,0,0,0,0,0',
+                'feedback' => 'Keep calm and sjekk ut billettservice.no'
+            ],
+            [
+                'text' => 'Næh, de var bedre før da de var indie i Bergen',
+                'score' => '0,0,0,0,0,0,0,0,0,0,0,0,0',
+                'feedback' => '90-tallet ringte. De vil gjerne ha grungejakka og tonefallet sitt tilbake'
+            ],
+            [
+                'text' => 'Hva er en Ylvis og hva er en OMG?',
+                'score' => '0,0,0,0,0,0,0,0,0,0,0,0,0',
+                'feedback' => 'Det er mye fint fjernsynsteater på NRK 2'
+            ]
+        ]
+    ]
+];
+?>
