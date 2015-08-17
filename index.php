6c0f5b8c9ebe3caf18327f72af5cb6b1154b3144
LarsJay <lars.johannessen@sbsdiscovery.no>
2015-08-17 15:59:11 +0200

    layout second draft

--- index.php
+++ index.php
@@ -4,53 +4,88 @@ require_once 'set_env.php';
 
 // 
 require_once 'language/'.$language.'/questions.php';
-?><!DOCTYPE html>
+?>
+
+<!doctype html>
+<!--[if lt IE 9]><html class="ie"><![endif]-->
+<!--[if gte IE 9]><!--><html><!--<![endif]-->
+
+<!--
+	The comment jumble above is handy for targeting old IE with CSS.
+	http://paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/
+-->
 <html>
 	<head>
-		<meta charset="UTF-8">
-		<title>Valgomat - hva DU bør se på TVNorge denne sesongen</title>
+		<meta charset="utf-8"/>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+		<title>Responsive template - animated and mobile menu</title>
 
+		<!-- Please don't add "maximum-scale=1" here. It's bad for accessibility. -->
 		<meta name="viewport" content="width=device-width, initial-scale=1"/>
-		 
-		<link rel="stylesheet" type="text/css" href="css/grid.css">
-		<link rel="stylesheet" type="text/css" href="css/style.css">
+
+		<link rel="stylesheet" href="css/grid.css"/>
+		<link rel="stylesheet" href="css/style.css"/>
 		
-		<!-- Grid overlay script. -->
+		<!-- <link rel="shortcut icon" href="favicon.ico" /> -->    
+		
+		<!-- Here's Golden Gridlet, the grid overlay script. -->
 		<script src="js/GGS.js"></script>
+				
+		<!-- 
+			This script enables structural HTML5 elements in old IE.
+			http://code.google.com/p/html5shim/
+		-->
+		<!--[if lt IE 9]>
+			<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
 		
-		<script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
-		<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
-		<script src="js/quizify.js"></script>
-		
-		
-	</head>
+		</head>
+
 	<body lang="no" ontouchstart="" onmouseover="">
-	<script>
-	  window.fbAsyncInit = function() {
-	          FB.init({
-	                        appId      : '1630666833841212',
-	                              xfbml      : true,
-	                                    version    : 'v2.3'
-	                                        });
 	
-	              // ADD ADDITIONAL FACEBOOK CODE HERE
-	            };
-	
-	  (function(d, s, id){
-	        var js, fjs = d.getElementsByTagName(s)[0];
-	             if (d.getElementById(id)) {return;}
-	                  js = d.createElement(s); js.id = id;
-	                       js.src = "//connect.facebook.net/en_US/sdk.js";
-	                            fjs.parentNode.insertBefore(js, fjs);
-	                               }(document, 'script', 'facebook-jssdk'));
-	</script>
-		<div id="questions" class="q-wrapper">
+		<script>
+		  window.fbAsyncInit = function() {
+		          FB.init({
+		                        appId      : '1630666833841212',
+		                              xfbml      : true,
+		                                    version    : 'v2.3'
+		                                        });
+		
+		              // ADD ADDITIONAL FACEBOOK CODE HERE
+		            };
+		
+		  (function(d, s, id){
+		        var js, fjs = d.getElementsByTagName(s)[0];
+		             if (d.getElementById(id)) {return;}
+		                  js = d.createElement(s); js.id = id;
+		                       js.src = "//connect.facebook.net/en_US/sdk.js";
+		                            fjs.parentNode.insertBefore(js, fjs);
+		                               }(document, 'script', 'facebook-jssdk'));
+		</script>
+
+		<header>
+			<section class="logo">
+				<img src="images/dnetworks@2x.png" width="122" height="52" alt="Discovery Networks logo" />
+			</section>
+		</header>
+		
+		<article class="oneway">
+			<section class="wrapper">
+						<div class="stepbystep-nav"></div>
+			</section>
+		</article>
+		
+				
+	<div id="questions" class="q-wrapper">
+		   
+		    
 		    <?php
 		    for( $i=0; $i<count($questions); $i++ ) {
 		        $question = $questions[$i];
 		    ?>
 		    <div id="question-<?php FlushValue($i + 1); ?>" class="question-container">
-		        <article class="oneway">
+    
+		        <article class="oneway wrapper">
 		        	<h1 class="question"><?php FlushValue($question['question']); ?></h1>
 		        </article>
 		        <article class="twoway">
@@ -61,48 +96,64 @@ require_once 'language/'.$language.'/questions.php';
 				            foreach( $alternatives as $alternative ) {
 				            	
 				            ?>
-				            <h3 class="alternative">
-				            <div class="text"><?php FlushValue($alternative['text']); ?></div>
+				            <div class="alternative">
+				            <h3 class="text">
+				            <input id="radio1" type="radio" name="radio" value="1" checked="checked"><label for="radio1"><span><span></span></span>Option 1</label>
+ 
+				            <a href="#"><?php FlushValue($alternative['text']); ?></a></h3>
 				                <div class="metadata"><?php FlushValue($alternative['score']); ?></div>
-				            </h3>		
+				            </div>		
 				            <?php
 				            }
 				            ?>
+						</div><!-- end alternatives -->
+			     </section>
+			      	<section class="wrapper">
+					    <figure >    
+					        <img class="wayimage" src="images/952x636.png" alt="imagetest">
+					    </figure>     
 				        </div>
 			        </section>
-			        <section class="wrapper">
-				    <figure class="wrapper">    
-				        <img class="wayimage" src="images/952x636.png" alt="imagetest">
-				    </figure>     
-				        </div>
-			        </section>
-		        </article>     
-		    </div>
+		        </article><!-- end twoway -->     
 		    <?php
 		    }
 		    ?>
-		</div>
-		<div id="feedbacks" class="f-wrapper">
-		    <?php
-		    for( $i=0; $i<count($questions); $i++ ) {
-		        $question = $questions[$i];
-	            $alternatives = $question['alternatives'];
-			    for( $j=0; $j<count($alternatives); $j++ ) {
-	            	$alternative = $alternatives[$j];
-	            	$feedback = $alternative["feedback"];
-	            ?>
-	            <div id="feedback-<?php FlushValue($i + 1); ?>-<?php FlushValue($j + 1); ?>" class="feedback-container"><?php FlushValue($feedback); ?></div>
-	            <?php
-	            }
-		    }
-		    ?>
-			<article class="oneway">
-				<section class="wrapper">
-						<div class="button" id=" ">	
-							<a href="#">Neste spørsmål!</a>
-						</div>		
-				</section>
-			</article>
-		</div>
+		    </div><!-- end question-container -->
+		    
+		</div> <!-- end q-wrapper --> 
+	
+		<article class="oneway">		
+			<section class="wrapper">
+				<div id="feedbacks" class="f-wrapper">
+				    <?php
+				    for( $i=0; $i<count($questions); $i++ ) {
+				        $question = $questions[$i];
+			            $alternatives = $question['alternatives'];
+					    for( $j=0; $j<count($alternatives); $j++ ) {
+			            	$alternative = $alternatives[$j];
+			            	$feedback = $alternative["feedback"];
+			            ?>
+			            <div id="feedback-<?php FlushValue($i + 1); ?>-<?php FlushValue($j + 1); ?>" class="feedback-container"><?php FlushValue($feedback); ?></div>
+			            <?php
+			            }
+				    }
+				    ?>
+				</div> <!-- end f-wrapper -->
+			</section>
+		</article>
+		
+		<!-- jQuery -->
+		<script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+		<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+		
+		<!-- Quizify -->
+		<script src="js/quizify.js"></script>
+
+		<!-- load Greensock TweenMax -->
+		<script src="http://cdnjs.cloudflare.com/ajax/libs/gsap/1.10.2/TweenMax.min.js"></script>
+		
+			
+					  	
 	</body>
-</html> 
+
+</html>
