cdbe1be3dcf59d268b28fa870b0aa5c9fb4a2cce
Hans-Petter Harveg <hanspetter@spillerom.no>
2015-08-30 13:26:14 +0200

    I HATE Facebook sharing..

--- index.php
+++ index.php
@@ -41,7 +41,18 @@ require_once 'set_env.php';
 		
 	</head>
 	<body class="start-end" lang="no">
- 		<header class="logo">
+        <!-- Load Facebook SDK for JavaScript -->
+        <div id="fb-root"></div>
+        <script>(function(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s); js.id = id;
+            js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1";
+            fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));
+        </script>
+
+		<header class="logo">
 			<img src="images/516x210_logo.png" width="258" height="105" alt="logo" />
 		</header>
 		
