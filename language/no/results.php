07ea4933423a97a1b7607515ac73326e248ca3c9
Hans-Petter Harveg <hanspetter@spillerom.no>
2015-08-31 10:59:34 +0200

    Fixed a typo

--- language/no/results.php
+++ language/no/results.php
@@ -3,15 +3,15 @@
 $results = array(
     // BRILLE:
     array('headline' => 'Ditt favorittprogram denne høsten er: <br><br> <strong>Brille</strong>',
-    'description' => 'Brille er et humorbasert panelprogram med noen av landets morsomste komikere. Harald Eia er programleder og Bård Tufte Johansen er fast paneldeltaker. Sesongpremiere 8. september, men husk også kommunevalget 14. september!.'),
+    'description' => 'Brille er et humorbasert panelprogram med noen av landets morsomste komikere. Harald Eia er programleder og Bård Tufte Johansen er fast paneldeltaker. Sesongpremiere 8. september, men husk også kommunevalget 14. september!'),
 
     // KJENDISKVELD MED MAGNUS DEVOLD:
     array('headline' => 'Ditt favorittprogram denne høsten er: <br><br> <strong>Kjendiskveld med Magnus Devold</strong>',
-    'description' => 'Etter fire år som praktikant for Ylvis-brødrene får Magnus Devold endelig sitt eget humorprogram. Premiere 8. september, men husk også kommunevalget 14. september!.'),
+    'description' => 'Etter fire år som praktikant for Ylvis-brødrene får Magnus Devold endelig sitt eget humorprogram. Premiere 8. september, men husk også kommunevalget 14. september!'),
 
     // I KVELD MED YLVIS LIVE:
     array('headline' => 'Ditt favorittprogram denne høsten er: <br><br> <strong>I kveld med YLVIS LIVE</strong>',
-    'description' => 'Ylvis-brødrene fortsetter det dumdristige prosjekt med å sende talk show på direkten. Det betyr at alt kan skje. Sesongpremiere er ubestemt, men husk uansett kommunevalget 14. september!.'),
+    'description' => 'Ylvis-brødrene fortsetter det dumdristige prosjekt med å sende talk show på direkten. Det betyr at alt kan skje. Sesongpremiere er ubestemt, men husk uansett kommunevalget 14. september!'),
 
     // ADAM SØKER EVA:
     array('headline' => 'Ditt favorittprogram denne høsten er: <br><br> <strong>Adam søker Eva</strong>',
@@ -19,27 +19,27 @@ $results = array(
 
     // EN HELT VANLIG DAG:
     array('headline' => 'Ditt favorittprogram denne høsten er: <br><br> <strong>En helt vanlig dag</strong>',
-    'description' => 'Odd-Magnus Williamson og Henrik Thodesen lager humor av norske humoristers hverdag. Premiere 15. september, men husk også kommunevalget dagen før!.'),
+    'description' => 'Odd-Magnus Williamson og Henrik Thodesen lager humor av norske humoristers hverdag. Premiere 15. september, men husk også kommunevalget dagen før!'),
 
     // ALT FOR NORGE:
     array('headline' => 'Ditt favorittprogram denne høsten er: <br><br> <strong>Alt for Norge</strong>',
-    'description' => 'Med to Gullruter under beltet er seerfavoritten tilbake for sjette gang med tolv eventyrlystne norsk-amerikanere på søndager 20.00, men husk også kommunevalget 14. september! .'),
+    'description' => 'Med to Gullruter under beltet er seerfavoritten tilbake for sjette gang med tolv eventyrlystne norsk-amerikanere på søndager 20.00, men husk også kommunevalget 14. september!'),
 
     // DANSKEN OG FINGEREN: BEST I ALT:
     array('headline' => 'Ditt favorittprogram denne høsten er: <br><br> <strong>Dansken og Fingern – best i alt</strong>',
-    'description' => 'Hvem er best til å føde av Dansken og Fingern? Det og mange andre helt unødvendige konkurranser. Premiere på MAX 15 september, men husk også kommunevalget dagen før!.'),
+    'description' => 'Hvem er best til å føde av Dansken og Fingern? Det og mange andre helt unødvendige konkurranser. Premiere på MAX 15 september, men husk også kommunevalget dagen før!'),
 
     // ÅNDENES MAKT:
     array('headline' => 'Ditt favorittprogram denne høsten er: <br><br> <strong>Åndenes makt</strong>',
-    'description' => 'Den ellevte sesongen innledes med et besøk på Dovrefjell hotell, der de ansatte plages av ting de ikke forstår. Sesongpremiere 13. september, men husk også kommunevalget dagen etterpå!.'),
+    'description' => 'Den ellevte sesongen innledes med et besøk på Dovrefjell hotell, der de ansatte plages av ting de ikke forstår. Sesongpremiere 13. september, men husk også kommunevalget dagen etterpå!'),
 
     // MONTEBELLO CAMPING:
     array('headline' => 'Ditt favorittprogram denne høsten er: <br><br> <strong>Montebello Camping</strong>',
-    'description' => 'Den familiedrevne campingplassen Montebello Camping smykker seg med slagordet «Innfallsporten til Europa». Premiere 14. september, husk også kommunevalget den samme dagen!.'),
+    'description' => 'Den familiedrevne campingplassen Montebello Camping smykker seg med slagordet «Innfallsporten til Europa». Premiere 14. september, husk også kommunevalget den samme dagen!'),
 
     // GIFT VED FØRSTE BLIKK:
     array('headline' => 'Ditt favorittprogram denne høsten er: <br><br> <strong>Gift ved første blikk</strong>',
-    'description' => 'I høst er det duket for en ny sesong av den omstridte realityserien som kan skilte med ett års bryllupsdag for et av fjorårets par. Sesongpremiere 17. september, men husk også kommunevalget 14. september!.'),
+    'description' => 'I høst er det duket for en ny sesong av den omstridte realityserien som kan skilte med ett års bryllupsdag for et av fjorårets par. Sesongpremiere 17. september, men husk også kommunevalget 14. september!'),
 
     // SINNASNEKKER'N:
     array('headline' => "Ditt favorittprogram denne høsten er: <br><br> <strong>Sinnasnekker'n</strong>",
@@ -47,11 +47,11 @@ $results = array(
 
     // 71 GRADER NORD:
     array('headline' => 'Ditt favorittprogram denne høsten er: <br><br> <strong>71° nord</strong>',
-    'description' => 'Til høsten leder eks-alpinist Tom Stiansen sin tiende sesong av realityflaggskipet 71° nord. Sesongpremiere 31. august, men husk også kommunevalget 14. september!.'),
+    'description' => 'Til høsten leder eks-alpinist Tom Stiansen sin tiende sesong av realityflaggskipet 71° nord. Sesongpremiere 31. august, men husk også kommunevalget 14. september!'),
 
     // PÅSKEEGG: FORBINDELSER PÅ DPLAY:
     array('headline' => 'Ditt favorittprogram denne høsten er: <br><br> <strong>Forbindelser</strong> på Dplay',
-    'description' => 'HHei. Dette ble litt rotete og rart, så algoritmen har blitt nødt til å følge sitt eget hjerte. Valget falt derfor på Nett-TV-tjenesten Dplay. For å se på Dplay trenger du abonnement. Send mail med skjermbilde av dette til kundeservice@sbsdiscovery.no og fortell at du har funnet påskeegget i TVNorges valgomat så spanderer vi Nett-TV på deg i et år. '),
+    'description' => 'HHei. Dette ble litt rotete og rart, så algoritmen har blitt nødt til å følge sitt eget hjerte. Valget falt derfor på Nett-TV-tjenesten Dplay. For å se på Dplay trenger du abonnement. Send mail med skjermbilde av dette til kundeservice@sbsdiscovery.no og fortell at du har funnet påskeegget i TVNorges valgomat så spanderer vi Nett-TV på deg i et år.'),
 
     // FEILMELDING:
     array('headline' => 'Feilmelding',
